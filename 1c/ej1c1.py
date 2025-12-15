c792da5bfb17a5f996e1521b146a7f9c928e6384
danieladlr-esp <daniel_adlr@hotmail.com>
2025-12-15 11:18:50 +0100

    Update ej1c1.py
    ejercicio resuelto

--- 1c/ej1c1.py
+++ 1c/ej1c1.py
@@ -47,7 +47,14 @@ Exemple:
 
 def mult_recursive(value, times):
     # Write here your code
-    pass
+ if times == 0:
+  return 0
+
+ return value + mult_recursive(value, times -1)
+ 
+ 
+ 
+ pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
